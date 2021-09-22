1a337ad2a0a099eb456955991bb31731a2f30590
Awtry <jorge_verah@outlook.es>
2021-09-22 02:05:49 -0500

    Ya esta ajustada
    Solo hace falta acomodar el los colores para que sean amigables

--- src/main/java/web/SERVLET.java
+++ src/main/java/web/SERVLET.java
@@ -26,21 +26,32 @@ public class SERVLET extends HttpServlet {
         out.print("<head>");
         out.print("<tittle>Respuesta Servlet</tittle>");
         out.print("<meta charset='UTF-8'>");
-        //out.print("<link rel='stylesheet' href='recursos/estilos.css'/>");
+        out.print("<link rel='stylesheet' href='recursos/estilos.css'/>");
         out.print("<link href='//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' rel='stylesheet'>");
         out.print("</head>");
-        out.print("<body class='perritosJamon'>");
-        out.print("<table width='200' class='table'>");
+        out.print("<body style = 'background-color: #2f1b3a'>");
         
-        out.print("<thead><tr><th scope='col'>Usuario</th>");
+        out.print("<div class='container' style = 'background-color: #e4ccf3; background:#f4c28a' text-aling = center >");
+        out.print("<h3>Respuesta Servlet</h3>");
+        out.print("</div>");
+        
+        //#e4ccf3
+        out.print("<div class='jumbotron' style = 'background-color: #e4ccf3'>");
+        out.print("<div class='container'>");
+
+        // Tabla solamente
+        out.print("<table class='table'>");
+        out.print("<thead style = 'background-color: #685de7' class='topTabla' id='topTabla'><tr><th scope='col'>Usuario</th>");
         out.print("<th scope='col'>Password</th>");
         out.print("<th scope='col'>Tecnología</th>");
         out.print("<th scope='col'>Genero</th>");
         out.print("<th scope='col'>Ocupación</th>");
         out.print("<th scope='col'>Música</th>");
         out.print("<th scope='col'>Comentarios</th></tr></thead>");
+
         
-        out.print("<tbody> <tr>");
+        //#ba4681
+        out.print("<tbody style = 'background-color: #d2a8ee'> <tr>");
         out.print("<td>" + usuario + "</td>");
         out.print("<td>" + password + "</td>");
 
@@ -72,101 +83,15 @@ public class SERVLET extends HttpServlet {
 
         out.print("</tr> </tbody>");
         out.print("</table>");
+        //Tabla
+        
+        out.print("</div>");
+        out.print("</div>");
+        
+        
         out.print("</body>");
         out.print("</html>");
 
-        /*
-        
-        <title>Formulario</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="recursos/estilos.css"/>
-        
-        <caption>Registro de datos</caption>
-                <tr>
-                    <td class="columna">
-                        Usuario:(*)
-                    </td>
-                    <td>
-                        <input class="default" type="text" name="usuario" value = "Escribir usuario" onfocus="this.select()">
-                    </td>
-                </tr>
-                <tr>
-                    <td class="columna">
-                        Password:(*)
-                    </td>
-                    <td>
-                        <input class="default" type="text" name="password" onfocus="this.select()">
-                    </td>
-                </tr>
-                <tr>
-                    <td class="columna">
-                        Tecnológias de desarrollo:(*)
-                    </td>
-                    <td>
-                        Java <input type="checkbox" name="tecnologia" value="java"><!-- comment -->
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        Do Net<input type="checkbox" name="tecnologia" value="net"><!-- comment -->
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        PHP <input type="checkbox" name="tecnologia" value="php">
-                    </td>
-                </tr>
-                <tr>
-                    <td class="columna">
-                        Genero:(*)
-                    </td>
-                    <td>
-                        Hombre: <input  type="radio" name="genero" value="H">
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        Mujer: <input  type="radio" name="genero" value="M">
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        Otro: <input  type="radio" name="genero" value="O">
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                    </td>
-                </tr>
-                <tr>
-                    <td class="columna">
-                        Ocupacion:(*)
-                    </td>
-                    <td>
-                        <select name="ocupacion" class="default">
-                            <option value="">Seleccionar</option>
-                            <option value="1">Profesor</option>
-                            <option value="2">Ingeniero</option>
-                            <option value="3">Artista</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="columna">
-                        Música favorita: 
-                    </td>
-                    <td>
-                        <select name="musica" multiple class="default">
-                            <option value="artcore">Artcore</option>
-                            <option value="electronica">Electronica</option>
-                            <option value="oriental">Oriental</option>
-                            <option value="orquesta">Orquesta</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="columna">
-                        Comentarios: 
-                    </td>
-                    <td>
-                        <textarea name="comentarios" rows="2" cols="30" onfocus="this.select()"></textarea>
-                    </td>
-                </tr>
-                <tr style="text-align: center">
-                    <td>
-                        <input type="reset" value="Limpiar" class="default"> 
-                    </td>
-                    <td>
-                        <input type="submit" value="Vamonos" class="default"> 
-                    </td>
-                </tr>
-            </table>
-         */
     }
 
 }
